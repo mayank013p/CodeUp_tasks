a593ba016aed05c32065271a0908a7808c16d7c7
Mayank Aitan <aitanmayank@gmail.com>
2024-09-03 12:21:15 +0530

    "init"

---
+++ Assisgn_1/src/CharFraString_Task2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CharFraString_Task2 {
+
+    public static String freqOfString(String input){
+    }
+
+    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String output = freqOfString(input);
+        System.out.println(output);
+    }
+}
